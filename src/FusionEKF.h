979a4ab7c3577bfa130bcad99b3194a626061572
Kiril Cvetkov <>
2018-11-14 07:07:12 +0100

    Added Lidar measurements state computation

--- src/FusionEKF.h
+++ src/FusionEKF.h
@@ -11,39 +11,49 @@
 
 class FusionEKF {
 public:
-  /**
-  * Constructor.
-  */
-  FusionEKF();
-
-  /**
-  * Destructor.
-  */
-  virtual ~FusionEKF();
-
-  /**
-  * Run the whole flow of the Kalman Filter from here.
-  */
-  void ProcessMeasurement(const MeasurementPackage &measurement_pack);
-
-  /**
-  * Kalman Filter update and prediction math lives in here.
-  */
-  KalmanFilter ekf_;
-
+    /**
+     * Constructor.
+     */
+    FusionEKF();
+    
+    /**
+     * Destructor.
+     */
+    virtual ~FusionEKF();
+    
+    /**
+     * Run the whole flow of the Kalman Filter from here.
+     */
+    void ProcessMeasurement(const MeasurementPackage &measurement_pack);
+    
+    /**
+     * Kalman Filter update and prediction math lives in here.
+     */
+    
+    
+    /**
+     * Laser function that help to calculate Q matrix parameters
+     */
+    float ProcessNoise(const float &dt, const float &sigma, const float &power);
+    
+    KalmanFilter ekf_;
+    
 private:
-  // check whether the tracking toolbox was initialized or not (first measurement)
-  bool is_initialized_;
-
-  // previous timestamp
-  long long previous_timestamp_;
-
-  // tool object used to compute Jacobian and RMSE
-  Tools tools;
-  Eigen::MatrixXd R_laser_;
-  Eigen::MatrixXd R_radar_;
-  Eigen::MatrixXd H_laser_;
-  Eigen::MatrixXd Hj_;
+    // check whether the tracking toolbox was initialized or not (first measurement)
+    bool is_initialized_;
+    
+    float noise_ax;
+    float noise_ay;
+    
+    // previous timestamp
+    long long previous_timestamp_;
+    
+    // tool object used to compute Jacobian and RMSE
+    Tools tools;
+    Eigen::MatrixXd R_laser_;
+    Eigen::MatrixXd R_radar_;
+    Eigen::MatrixXd H_laser_;
+    Eigen::MatrixXd Hj_;
 };
 
 #endif /* FusionEKF_H_ */
